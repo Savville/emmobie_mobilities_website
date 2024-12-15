beefdc67915ad8f7875a95f80cfa4fbe595c8159
WILLIAMS OCHIENG <143290413+Savville@users.noreply.github.com>
2024-12-15 00:32:21 -0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import {React} from 'react';
+
+import './app.jsx';
+import BicycleRentingApp from './app.jsx';
+
+
+const App = () => {
+  return (
+    <div className='App'>
+        <BicycleRentingApp/>
+   
+    </div>
+  )
+}
+
+export default App
